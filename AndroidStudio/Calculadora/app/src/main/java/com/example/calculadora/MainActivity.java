242af85287dc0d790cf2629b6b46e8aa6892ff9f
RogelioClase <10377573@ieselgrao.org>
2022-10-25 12:21:19 +0200

    Grao Computer: 25-10-2022

--- AndroidStudio/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ AndroidStudio/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
