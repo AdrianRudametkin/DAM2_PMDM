80723551e1f4f0cd878ba66ce8d470e14ba6f64a
RogelioClase <10377573@ieselgrao.org>
2022-11-24 09:57:12 +0100

    Grao Computer: 24-11-2022

--- AndroidStudio/CalculadoraSwitch/app/src/main/java/com/example/calculadoraswitch/MainActivity.java
+++ AndroidStudio/CalculadoraSwitch/app/src/main/java/com/example/calculadoraswitch/MainActivity.java
@@ -1,3 +1,4 @@
+//Hecho por Rogelio Rodriguez
 package com.example.calculadoraswitch;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +14,17 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Creamos las variables que vayamos a utilizar
     Switch switch1;
     EditText et1, et2;
     TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Las vinculamos con el XML
         switch1 = findViewById(R.id.switch1);
         et1 = findViewById(R.id.et1);
         et2 = findViewById(R.id.et2);
@@ -28,24 +32,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View v){
+        // Si cuando se activa el switch, los textedit estan vacios...
         if(et1.getText().toString().isEmpty() || et2.getText().toString().isEmpty()){
+            // Notificar al usuario de su error
             Toast.makeText(this, "Tienes que poner un número...", Toast.LENGTH_SHORT).show();
+            // Apaga el switch
             switch1.setChecked(false);
+            // Salir del metodo
             return;
         }
 
         try {
+            // Sacamos los valores de los edit text
             double n1 = Double.parseDouble(et1.getText().toString());
             double n2 = Double.parseDouble(et2.getText().toString());
 
+            // Si cuando se pulsa el switch esta encendido...
             if (switch1.isChecked()) {
+                // Mostramos la suma
                 textView.setText(String.format(Locale.UK,"%.2f", n1+n2));
+            // Si esta apagado...
             }else{
+                // Mostramos un 0
                 textView.setText("0");
+                // Y borramos los números introducidos
                 et1.setText("");
                 et2.setText("");
             }
         }catch(NumberFormatException nfe){
+            // Si el número introducido es incorrecto, notificar al usuario
             nfe.printStackTrace();
             Toast.makeText(this, "Mal...", Toast.LENGTH_SHORT).show();
             switch1.setChecked(false);
